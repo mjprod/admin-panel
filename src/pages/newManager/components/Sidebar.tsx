a6777c73789b5997f1affe74f33d138d467f32fe
Mudra <pmudra12@gmail.com>
2025-03-10 15:59:40 +1000

    filter conservation added

--- src/pages/newManager/components/Sidebar.tsx
+++ src/pages/newManager/components/Sidebar.tsx
@@ -1,59 +1,28 @@
 import React from "react";
 import styles from "./Sidebar.module.css";
 import QuestionTools, { CategoryProps } from "./QuestionTools";
-import { TagColor } from "../../../components/tags/Tag";
 import SideCard from "./SideCard";
 import CreateNewButton from "./CreateNewButton";
 import { QuestionStatus } from "../../../util/QuestionStatus";
 
 interface SidebarProps {
   onSideCardClicked: (status: QuestionStatus) => void;
+  categories: CategoryProps[];
+  onCategoryClick: (category: CategoryProps) => void;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ onSideCardClicked }) => {
-  const categories: CategoryProps[] = [
-    {
-      title: "All",
-      number: 24000,
-      color: TagColor.ALL,
-      isSelected: true,
-    },
-    {
-      title: "4D Lotto",
-      number: 1356,
-      color: TagColor.GOLDISH,
-      isSelected: false,
-    },
-    {
-      title: "3rd Party",
-      number: 1356,
-      color: TagColor.PINK,
-      isSelected: false,
-    },
-    {
-      title: "Account",
-      number: 136,
-      color: TagColor.PURPLE,
-      isSelected: false,
-    },
-    {
-      title: "finance",
-      number: 396,
-      color: TagColor.GREEN,
-      isSelected: false,
-    },
-
-    {
-      title: "Lucky7",
-      number: 972,
-      color: TagColor.NAVY_BLUE,
-      isSelected: false,
-    },
-  ];
-
+const Sidebar: React.FC<SidebarProps> = ({
+  onSideCardClicked,
+  categories,
+  onCategoryClick,
+}) => {
   return (
     <aside className={styles["sidebar"]}>
-      <QuestionTools total={24000} categories={categories} />
+      <QuestionTools
+        total={24000}
+        categories={categories}
+        onCategoryClick={onCategoryClick}
+      />
 
       <div className={styles["list-container"]}>
         <CreateNewButton />
