8c503b2c38df5a762f6382919ba97ed8c0278669
Mudra <mudra.mjpro@gmail.com>
2025-03-11 15:40:19 +1000

    Refresh token created

--- src/api/axios-config.ts
+++ src/api/axios-config.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { showConsoleError, showConsoleMessage } from "../util/ConsoleMessage";
 import { AuthErrors } from "../context/AuthContext.js";
+import useRefreshToken from "./RefreshToken";
+/* eslint-disable complexity */
 
 export const setupInterceptors = (setLoading: (value: boolean) => void) => {
-  // Request Interceptor
+  const { refresh } = useRefreshToken()
   Request.interceptors.request.use(
     (config) => {
       setLoading(true); 
@@ -22,22 +24,39 @@ export const setupInterceptors = (setLoading: (value: boolean) => void) => {
     }
   );
 
-  // Response Interceptor
   Request.interceptors.response.use(
     (response) => {
-      setLoading(false); 
+      setLoading(false);
       showConsoleMessage("Response:", response);
-
       return response;
     },
-    (error) => {
+    async (error) => {
       setLoading(false);
       showConsoleError("Response Error: ", error.response || error.message);
+
+      const originalRequest = error.config;
+      
+      if ((error?.response?.status === 401 || error?.response?.status === 403) && !originalRequest._retry) {
+        originalRequest._retry = true; 
+        try {
+          const newToken = await refresh();
+          if (newToken) {
+            originalRequest.headers["Authorization"] = `Token ${newToken}`;
+            return Request(originalRequest); 
+          }
+        } catch (refreshError) {
+          console.error("Refresh token failed:", refreshError);
+          return Promise.reject(refreshError);
+        }
+      }
+
       const errorMsg: AuthErrors = { data: { error: error.response || error.message || "Unknown error", status: -1 } };
+
       return Promise.reject(errorMsg);
     }
   );
 };
+
 
 export const Request = axios;
 export default axios;
