62f8aee192d90f85e560b1a41e6bae6dea56dfca
Mudra <mudra.mjpro@gmail.com>
2025-03-13 12:49:43 +1000

    data map

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,7 @@
-import { Endpoint } from "./contants";
-import { KnowledgeResponse } from "./responsePayload/KnowledgeResponse";
+import { LanguageProps } from "../components/language/Language";
+import { getLanguageByCode, getLanguageById } from "../util/ExtensionFunction";
+import { DEFAULT_LANGUAGE_CODE, Endpoint } from "./contants";
+import { ConversationKnowledge, KnowledgeStatus, KnowledgeCard, KnowledgeResponse } from "./responsePayload/KnowledgeResponse";
 import {
   apiDeleteRequest,
   apiGetRequest,
@@ -19,23 +21,114 @@ export const UpdateKnowledge = async (id: string): Promise<string | null> => {
 export const AllConversation = async (
   pathVariables: Record<string, any> = {},
   queryParams: Record<string, any> = {}
-): Promise<KnowledgeResponse | null> => {
-  const isMock = true;
+): Promise<ConversationKnowledge | null> => {
+  const isMock = false;
 
-  if (isMock) {
-    const response = await fetch("/assets/conversations.json");
-    if (!response.ok) {
-      throw new Error("Failed to load mock data");
+  try {
+    if (isMock) {
+      const response = await fetch("/assets/conversations.json");
+      
+      if (!response.ok) {
+        throw new Error("Failed to load mock data: " + response.statusText);
+      }
+      const data = await response.json();
+      return mapKnowledgeConversationData(data);
+    } else {
+      const apiResponse = await apiGetRequest<KnowledgeResponse>(
+        Endpoint.Knowledge,
+        pathVariables,
+        queryParams
+      );
+      
+      if (!apiResponse) {
+        throw new Error("Failed to fetch data from the API.");
+      }
+
+      return mapKnowledgeConversationData(apiResponse);
     }
-    return await response.json();
-  } else {
-    return await apiGetRequest<KnowledgeResponse>(
-      Endpoint.Knowledge,
-      pathVariables,
-      queryParams 
-    );
+  } catch (error) {
+    console.error("Error in AllConversation:", error);
+    return null;
   }
+  
 };
+
+const mapKnowledgeConversationData = (response: KnowledgeResponse): ConversationKnowledge => {
+  console.log(`----id: ${response.count}`);
+
+  const knowledgeinfo: KnowledgeCard[] = [];
+
+
+  response.results.map((item) => {
+   
+    const knowledgeContent = item.knowledge_content.find((con) => con.language == getLanguageByCode(DEFAULT_LANGUAGE_CODE).id)
+
+    const langStatus: LanguageProps[] = item.knowledge_content.map((lang) => ({
+      id: lang.language,
+      lang: getLanguageById(lang.language).code,
+      langLabel: getLanguageById(lang.language).label,
+      isSolid: lang.language ==  getLanguageByCode(DEFAULT_LANGUAGE_CODE).id,
+      isCompleted: lang.status == KnowledgeStatus.Approved,
+      status: KnowledgeStatus[lang.status] 
+    }))
+
+    
+    if (knowledgeContent != null) {
+      const status: KnowledgeStatus = (() => {
+        switch (knowledgeContent.status) {
+          case 1:
+            return KnowledgeStatus.NeedReview;
+          case 2:
+            return KnowledgeStatus.PreApproved;
+          case 3:
+              return KnowledgeStatus.Approved;
+          case 4:
+            return KnowledgeStatus.Rejected;
+          default:
+            return KnowledgeStatus.Approved;
+        }
+      })();
+
+      const lang: LanguageProps = {
+        id: knowledgeContent.language,
+        lang: getLanguageById(knowledgeContent.language).code,
+        langLabel: getLanguageById(knowledgeContent.language).label,
+        isSolid: knowledgeContent.language ==  getLanguageByCode(DEFAULT_LANGUAGE_CODE).id,
+        isCompleted: knowledgeContent.status == KnowledgeStatus.Approved,
+        status: KnowledgeStatus[knowledgeContent.status] 
+      }
+
+      knowledgeinfo.push({
+        knowledgeId: item.id,
+        conversationId: item.knowledge_uuid,
+        category: item.category ? item.category : null,
+        subcategories: item.subcategory ? item.subcategory : null,
+        id: knowledgeContent.id,
+        dateTime: knowledgeContent.last_updated,
+        languages: langStatus,
+        currentlang: lang,
+        question: knowledgeContent.question,
+        answer: knowledgeContent.answer,
+        isEdited: knowledgeContent.is_edited,
+        inBrain: knowledgeContent.in_brain,
+        status: status
+        
+      });
+    }
+
+  })
+
+
+  return {
+    count: response.count,
+    total_pages: response.total_pages,
+    current_page: response.current_page,
+    next: response.next,
+    previous: response.previous,
+    data: knowledgeinfo,
+  };
+  
+}
 
 export const AddLanguageReviewed = async (
   docId: string,
