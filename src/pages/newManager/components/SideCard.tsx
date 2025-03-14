9da39bd26b0c7f25e390ad4ac3dd92b6311613c4
Effie-Dong <effie.mjpro@gmail.com>
2025-03-14 12:44:15 +1000

    removed number for different status from side bar

--- src/pages/newManager/components/SideCard.tsx
+++ src/pages/newManager/components/SideCard.tsx
@@ -16,7 +16,7 @@ interface SideCardProps {
 const SideCard: React.FC<SideCardProps> = ({
   isActive,
   status,
-  number,
+  // number,
   onSideCardClicked,
   classNameStyle,
 }) => {
@@ -45,7 +45,7 @@ const SideCard: React.FC<SideCardProps> = ({
       onClick={() => onSideCardClicked(status)}
     >
       <div className={styles["row01"]}>
-        <p>{number}</p>
+        {/* <p>{number}</p> */}
       </div>
       <div className={styles["row02"]}>
         <div
