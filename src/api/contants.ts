52d4205ea83caed8da4efd76801ca15a96aafa7d
GlaucoInfinities <162525681+GlaucoInfinities@users.noreply.github.com>
2025-03-14 20:54:48 +1000

    Update contants.ts

--- src/api/contants.ts
+++ src/api/contants.ts
@@ -1,8 +1,8 @@
 //API Domain Name
 import { showConsoleMessage } from "../util/ConsoleMessage";
 // export const apiDomainName = "api-staging.mjproapps.com/api";
-export const apiDomainName = "localhost:8000/api";
-
+//export const apiDomainName = "localhost:8000/api";
+export const apiDomainName = "13.236.193.147:8000/api";
 export const BASE_URI = `http://${apiDomainName}`;
 
 export const getBaseUri = () => {
@@ -11,7 +11,6 @@ export const getBaseUri = () => {
 };
 
 export const Endpoint = {
-
   Knowledge: `${getBaseUri()}/knowledge`,
   KnowledgeContent: `${getBaseUri()}/knowledge-content/{id}/`,
   KnowledgeContentBulkUpdate: `${getBaseUri()}/knowledge-content/bulk-update-status/`,
@@ -19,11 +18,9 @@ export const Endpoint = {
   KnowledgeContentBulkDelete: `${getBaseUri()}/knowledge-content/bulk-delete/`,
   KnowledgeSummary: `${getBaseUri()}/knowledge-summary/`,
 
-
   Category: `${getBaseUri()}/categories/`,
   SubCategory: `${getBaseUri()}/subcategories/`,
   CreateKnowledge: `${getBaseUri()}/knowledge-content/`,
-
 };
 
 //API Constants
@@ -33,7 +30,6 @@ export const notificationToken = String(
 export const sha256Salt = String(process.env.REACT_APP_SHA256SALT);
 export const secretKey = String(process.env.REACT_APP_SECRET_KEY);
 export const apiKey = String(process.env.REACT_APP_API_KEY || "");
-
 
 export const DEFAULT_LANGUAGE_CODE = "cn";
 export const DEFAULT_LANGUAGE_ID = 3;
