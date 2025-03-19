d0495f6cfaecad7e83f72cdf2c559954fb032279
Effie <effie.mjpro@gmail.com>
2025-03-19 19:35:11 +1000

    changed domainname

--- src/api/contants.ts
+++ src/api/contants.ts
@@ -1,9 +1,7 @@
 //API Domain Name
 import { showConsoleMessage } from "../util/ConsoleMessage";
-// export const apiDomainName = "api-staging.mjproapps.com/api";
-export const apiDomainName = "localhost:8000/api";
-// export const apiDomainName = "13.236.193.147:8000/api";
-export const BASE_URI = `http://${apiDomainName}`;
+export const apiDomainName = "api-staging.mjproapps.com/api";
+export const BASE_URI = `https://${apiDomainName}`;
 
 export const getBaseUri = () => {
   showConsoleMessage("Using Default API URL:", BASE_URI);
