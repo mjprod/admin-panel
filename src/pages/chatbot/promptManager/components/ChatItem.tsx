c1eac7f41b7ebbc043728ac3de3d0912f226c7db
MMaxleeJ 4 <maxlee@Chris-Taylor.local>
2025-06-19 15:23:02 +1000

    apply api integrated

--- src/pages/chatbot/promptManager/components/ChatItem.tsx
+++ src/pages/chatbot/promptManager/components/ChatItem.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import styles from "./ChatItem.module.css";
 import clsx from "clsx";
 import { PromptDataModel } from "./History";
+import { PromptPatch } from "../../../../api/apiCalls";
+import { useAppDispatch } from "../../../../store/hooks";
+import { updateConfirmationDialog } from "../../../../store/prompt.slice";
 
 interface ChatItemProps {
     chat: PromptDataModel;
@@ -9,6 +12,32 @@ interface ChatItemProps {
 
 const ChatItem: React.FC<ChatItemProps> = ({ chat }) => {
     const [expanded, setExpanded] = useState(false);
+    const dispatch = useAppDispatch()
+
+    const applyApicall = async () => {
+        if (!chat.is_default) {
+            try {
+                const response = await PromptPatch(chat.id, chat.node_name, chat.prompt, true);
+                console.log("Prompt Patch Response:", response)
+            } catch (error) {
+                console.error("Failed to patch data:", error);
+            }
+        } else {
+            // dispatch(updateConfirmationDialog(true))
+            console.error("Default---");
+
+            // setShowDialogConfirm(true)
+        }
+    };
+    const handleApply = () => {
+        applyApicall()
+    }
+
+    const handleEdit = () => {
+
+    }
+
+
 
     return (
         <div className={styles.card}>
@@ -17,8 +46,8 @@ const ChatItem: React.FC<ChatItemProps> = ({ chat }) => {
                 {expanded ? chat.prompt : `${chat.prompt.slice(0, 1000)}...`}
             </div>
             <div className={styles.actions}>
-                <button className={clsx(styles.button, styles.apply)} >Apply</button>
-                <button className={clsx(styles.button, styles.edit)} >Edit</button>
+                <button className={clsx(styles.button, styles.apply)} onClick={handleApply}>Apply</button>
+                <button className={clsx(styles.button, styles.edit)} onClick={handleEdit}>Edit</button>
                 <button className={clsx(styles.button, styles.expand)} onClick={() => setExpanded((prev) => !prev)}>   {expanded ? "Collapse" : "Expand"}</button>
 
 
