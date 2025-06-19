17cb1b0ebc5d9e11f7bf0465ee015440649ded7f
MMaxleeJ 4 <maxlee@Chris-Taylor.local>
2025-06-19 15:21:03 +1000

    apply api

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { onConversationListSlice } from "./conversation.slice";
 import paginationReducer from "./pagination.slice";
 import { onContextSlice } from "./context.slice";
+import { onPromptSlice } from "./prompt.slice";
 
 const store = configureStore({
   reducer: {
     conversation: onConversationListSlice.reducer,
     pagination: paginationReducer,
     context: onContextSlice.reducer,
+    prompt: onPromptSlice.reducer
   },
 });
 
